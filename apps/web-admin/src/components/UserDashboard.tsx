6d7dd8592bb58eef956f834d5a4a546196fd86fa
velomano <sky3rain7@gmail.com>
2025-10-01 12:04:44 +0900

    UserDashboard.tsx JSX 구조 최종 수정
    - 996번째 줄의 잘못된 위치에 있던 </div> 제거
    - 올바른 JSX 태그 구조로 수정하여 파싱 오류 완전 해결
    - 이제 빌드가 정상적으로 작동할 것

--- apps/web-admin/src/components/UserDashboard.tsx
+++ apps/web-admin/src/components/UserDashboard.tsx
@@ -992,8 +992,6 @@ export default function UserDashboard({ user, farms, devices, sensors, sensorRea
                         )}
                     </div>
                   </div>
-
-            </div>
           </div>
         </div>
 
